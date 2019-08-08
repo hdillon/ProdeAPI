45b82d8b0156111b4cbb358f99e0c831e3b16666
hdillon <dillonhoraciodavid@gmail.com>
2019-08-08 15:26:15 -0300

    Project creation

---
+++ src/main/java/com/solution/prode/ProdeApplication.java
@@ -0,0 +1,13 @@
+package com.solution.prode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdeApplication.class, args);
+	}
+
+}
