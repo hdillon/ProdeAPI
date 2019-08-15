8cf66780444ebce8495f18eb06155f2cf83f543c
hdillon <dillonhoraciodavid@gmail.com>
2019-08-15 20:09:49 -0300

    Rename initial sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO role(name) VALUES('ROLE_USER');
+INSERT IGNORE INTO role(name) VALUES('ROLE_ADMIN');
+
+INSERT IGNORE INTO team(name) VALUES('Argentina');
+INSERT IGNORE INTO team(name) VALUES('Brasil');
+INSERT IGNORE INTO team(name) VALUES('Uruguay');
+INSERT IGNORE INTO team(name) VALUES('Chile');
+INSERT IGNORE INTO team(name) VALUES('Peru');
